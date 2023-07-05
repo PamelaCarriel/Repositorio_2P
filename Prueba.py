e9a019cadb333f69c550fefd3584b9c4019d4fa2
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-07-05 09:35:17 -0500

    P

---
+++ Prueba.py
@@ -0,0 +1,16 @@
+#Universidad de la Fuerzas Armadas-ESPE
+#Autor:Pamela Jesabel Carriel Mier 
+"""Aplicar el algoritmo de ordenación de selección a la siguiente lista de números: [9, 2, 5, 1, 7]"""
+#Fecha : 05 de julio del 2023
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+lista = [9, 2, 5, 1, 7]
+resultado = selection_sort(lista)
+print(resultado)
--- Árboles/Buscar_Valor.py
+++ Árboles/Buscar_Valor.py
@@ -24,10 +24,10 @@ def insertar(arbol, valor):
             arbol.derecha = insertar(arbol.derecha, valor)
         return arbol
 
-arbol = insertar(arbol, 10)
+"""arbol = insertar(arbol, 10)
 arbol = insertar(arbol, 13)
 arbol = insertar(arbol, 16)
-arbol = insertar(arbol, 19)
+arbol = insertar(arbol, 19)"""
 
 def buscar_valor(arbol, valor):
     if arbol is None:
@@ -39,4 +39,4 @@ def buscar_valor(arbol, valor):
     else:
         return buscar_valor(arbol.derecha, valor)
 
-print(buscar_valor(arbol , 10))
+print(buscar_valor(arbol , 1))
